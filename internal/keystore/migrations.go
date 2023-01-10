952535ca2f94f3048f4043fc6efb7c27e928cb39
Mohammed Sohail <sohailsameja@gmail.com>
2023-01-10 11:09:54 +0000

    add: keystore tests

--- internal/keystore/migrations.go
+++ internal/keystore/migrations.go
@@ -8,7 +8,7 @@ import (
 )
 
 func applyMigration(dbPool *pgxpool.Pool) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 
 	_, err := dbPool.Exec(ctx, `
